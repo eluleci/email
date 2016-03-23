182778a89c518968cd330479d354d660445bf7f4
Emrullah Lüleci <emrullahluleci@gmail.com>
2016-03-24 01:09:02 +0200

    fix bug related to the hidden body issue when attachment provided

--- email.go
+++ email.go
@@ -124,7 +124,7 @@ func (m *Message) Bytes() []byte {
 	boundary := "f46d043c813270fc6b04c2d223da"
 
 	if len(m.Attachments) > 0 {
-		buf.WriteString("Content-Type: multipart/mixed; boundary=" + boundary + "\r\n")
+		buf.WriteString("Content-Type: multipart/mixed; boundary=" + boundary + "\r\n\n")
 		buf.WriteString("--" + boundary + "\r\n")
 	}
 
